e745ad3c99c5611a6f31a618438226d591f346a1
localusercamp <user062199@gmail.com>
2021-03-01 13:00:56 +0500

    changes tabs

---
+++ spa/src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="custom-header">
+    <div class="navlink">
+      <a href="/back-end">Backend</a>
+    </div>
+    <div class="navlink">
+      <a href="/cli">CLI</a>
+    </div>
+    <div class="navlink">
+      <a href="/front-end">Frontend</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+
+  data() {
+    return {
+      show: true,
+    }
+  },
+}
+</script>
+
+<style>
+  .custom-header {
+    width: 100%;
+    height: 50px;
+    border-bottom: 1px solid lightgrey;
+  }
+  .navlink {
+    font-family: 'Josefin Sans', sans-serif;
+    font-size: 36px;
+    display: inline-block;
+    border-top: 1px solid lightgrey;
+    border-left: 1px solid lightgrey;
+    border-right: 1px solid lightgrey;
+    height: 49px;
+    padding: 8px 20px 0 20px;
+  }
+  .navlink > a {
+    text-decoration: none;
+    color: black;
+  }
+</style>
--- spa/src/router/index.js
+++ spa/src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Scheme from '@/components/Scheme'
+import Backend from '@/components/Backend'
+import CLI from '@/components/CLI'
+import Frontend from '@/components/Frontend'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Scheme',
-      component: Scheme
+      path: '',
+      name: 'home',
+      // component: Backend,
+      redirect: { name: 'backend' },
+    }, {
+      path: '/back-end',
+      name: 'backend',
+      component: Backend
+    }, {
+      path: '/cli',
+      name: 'cli',
+      component: CLI
+    }, {
+      path: '/front-end',
+      name: 'frontend',
+      component: Frontend
     }
   ]
 })
